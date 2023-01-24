deb3da3bb703b69e41801c2babdfa25453354d96
O Y <orenyaniv90@gmail.com>
2023-01-25 00:20:34 +0200

    [fix] Change weather api key

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,22 +1,22 @@
-// import axios from "axios"
-// import { weatherKey } from "../aws-exports"
+import axios from "axios"
 
-// const AXIOS = axios.create()
+
+const AXIOS = axios.create()
 
 async function get(location: string) {
     try {
         // For development: uncomment beneath lines to avoid api call
-        if (Math.random() > 0.9) throw new Error('Dev error')
-        return {
-            temp: 10,
-            icon: 'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'
-        }
-
-        // const { data } = await AXIOS.get(`http://api.weatherstack.com/current?access_key=${weatherKey}&query=${location}`)
+        // if (Math.random() > 0.9) throw new Error('Dev error')
         // return {
-        //     temp: data.current.temperature,
-        //     icon: data.current.weather_icons[0]
+        //     temp: 10,
+        //     icon: 'https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'
         // }
+
+        const { data } = await AXIOS.get(`http://api.weatherstack.com/current?access_key=d948ebdabb7c3f28fd09c318c7e2f52e&query=${location}`)
+        return {
+            temp: data.current.temperature,
+            icon: data.current.weather_icons[0]
+        }
     } catch (err) {
         throw err
     }
